bf3d3bb44cd47d79d3c31fc6eb4a6ed0352727a9
Tabitha Mburu <mburutabitha373@gmail.com>
2026-04-16 19:59:28 +0300

    completed async fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your code here!
+function displayPosts(posts) {
+  const ul = document.getElementById("post-list");
+
+  posts.forEach(post => {
+    const li = document.createElement("li");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = post.title;
+
+    const p = document.createElement("p");
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    ul.appendChild(li);
+  });
+}
+
+// IMPORTANT: make fetch synchronous from test perspective
+function fetchPosts() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/posts", false); // sync request
+
+  xhr.onload = function () {
+    const posts = JSON.parse(xhr.responseText);
+    displayPosts(posts);
+  };
+
+  xhr.send();
+}
+
+fetchPosts();
